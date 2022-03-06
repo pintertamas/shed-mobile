02eb8af4edc35997419bd655e4bf7ff951b980af
Tomi <pintertamas99@gmail.com>
2022-03-06 19:44:32 +0100

    websocket refactor

--- lib/screens/my_app.dart
+++ lib/screens/my_app.dart
@@ -14,8 +14,33 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  late StompClient stompClient;
+  String message = "";
+
   @override
   void initState() {
+    stompClient = StompClient(
+      config: StompConfig.SockJS(
+        url: 'https://192.168.0.34:8443/shed',
+        onConnect: onConnect,
+        onDisconnect: onDisconnect,
+        onStompError: (error) {
+          print("stompError: " + error.toString());
+        },
+        onUnhandledMessage: (error) {
+          print("unhandledError: " + error.toString());
+        },
+        beforeConnect: () async {
+          print('waiting to connect...');
+          await Future.delayed(Duration(milliseconds: 200));
+          print('connecting...');
+        },
+        onWebSocketError: (dynamic error) => print(error.toString()),
+        stompConnectHeaders: {'Authorization': 'Bearer yourToken'},
+        webSocketConnectHeaders: {'Authorization': 'Bearer yourToken'},
+      ),
+    );
+
     stompClient.activate();
     super.initState();
   }
@@ -24,8 +49,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor:
-            Colors.deepPurple,
+        backgroundColor: Colors.deepPurple,
         body: Center(
           child: TextButton(
             onPressed: _sendMessage,
@@ -33,7 +57,7 @@ class _MyAppState extends State<MyApp> {
               backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
             ),
             child: Text(
-              "Click",
+              message == "" ? "Click" : message,
               textDirection: TextDirection.ltr,
               style: TextStyle(
                 color: Colors.black,
@@ -54,54 +78,34 @@ class _MyAppState extends State<MyApp> {
     print(stompClient.config.url);
   }
 
+  void onConnect(StompFrame frame) {
+    String gameId = 'asd';
+    stompClient.subscribe(
+      destination: '/topic/' + gameId,
+      callback: (frame) {
+        print("receiving message...");
+        var result = json.decode(frame.body!);
+        message = result['message'];
+        setState(() {});
+        print(result);
+      },
+    );
+
+    Timer.periodic(Duration(seconds: 1), (_) {
+      stompClient.send(
+        destination: 'topic/asd',
+        body: json.encode({'a': 123}),
+      );
+    });
+  }
+
+  void onDisconnect(StompFrame frame) {
+    print("Disconnecting...");
+  }
+
   @override
   void dispose() {
     stompClient.deactivate();
     super.dispose();
   }
 }
-
-void onConnect(StompFrame frame) {
-  String gameId = 'asd';
-  stompClient.subscribe(
-    destination: '/topic/' + gameId,
-    callback: (frame) {
-      print("receiving message...");
-      var result = json.decode(frame.body!);
-      print(result);
-    },
-  );
-
-  Timer.periodic(Duration(seconds: 1), (_) {
-    stompClient.send(
-      destination: 'topic/asd',
-      body: json.encode({'a': 123}),
-    );
-  });
-}
-
-void onDisconnect(StompFrame frame) {
-  print("Disconnecting...");
-}
-
-final stompClient = StompClient(
-  config: StompConfig.SockJS(
-    url: 'https://192.168.0.34:8443/shed',
-    onConnect: onConnect,
-    onDisconnect: onDisconnect,
-    onStompError: (error) {
-      print("stompError: " + error.toString());
-    },
-    onUnhandledMessage: (error) {
-      print("unhandledError: " + error.toString());
-    },
-    beforeConnect: () async {
-      print('waiting to connect...');
-      await Future.delayed(Duration(milliseconds: 200));
-      print('connecting...');
-    },
-    onWebSocketError: (dynamic error) => print(error.toString()),
-    stompConnectHeaders: {'Authorization': 'Bearer yourToken'},
-    webSocketConnectHeaders: {'Authorization': 'Bearer yourToken'},
-  ),
-);
