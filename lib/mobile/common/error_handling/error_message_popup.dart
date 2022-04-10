82c914c2ed7741c393acc6a2ab2cd9f9d753ec55
Pinter Tamas <pintertamas99@gmail.com>
2022-04-10 15:36:40 +0200

    error message popup

--- lib/mobile/common/error_handling/error_message_popup.dart
+++ lib/mobile/common/error_handling/error_message_popup.dart
@@ -1,17 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 Future errorMessagePopup(BuildContext context, String errorMessage) {
   return showDialog(
     context: context,
     builder: (BuildContext context) {
       return AlertDialog(
-        backgroundColor: Colors.red,
-        content: Text(
-          errorMessage,
-          style: const TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-            fontSize: 25,
+        backgroundColor: Colors.transparent,
+        content: Container(
+          decoration: const BoxDecoration(
+            color: Colors.red,
+            borderRadius: BorderRadius.all(Radius.circular(15.0)),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(top: 10.0, bottom: 20.0),
+                  child: Icon(
+                    FontAwesomeIcons.exclamationTriangle,
+                    size: 25,
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  errorMessage,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
           ),
         ),
       );
