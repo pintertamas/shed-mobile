5529483e30dd26e88f700c2f926a927fed5acd1e
Tomi <pintertamas99@gmail.com>
2022-04-25 00:04:20 +0200

    browse games on the web starter page see connected users on the web qr screen browse games on the mobile brose game page bug: connectedplayerprovider breaks the code

--- lib/main.dart
+++ lib/main.dart
@@ -2,99 +2,51 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:websocket_mobile/mobile/game/model/game_screen_arguments.dart';
-import 'package:websocket_mobile/mobile/game/screen/game_screen.dart';
-import 'package:websocket_mobile/mobile/lobby/model/lobby_screen_arguments.dart';
+import 'package:provider/provider.dart';
+import 'package:websocket_mobile/mobile/common/provider/connected_player_provider.dart';
+import 'package:websocket_mobile/mobile/common/routing/route_generator.dart';
 import 'package:websocket_mobile/mobile/lobby/screen/browse_games.dart';
-import 'package:websocket_mobile/mobile/lobby/screen/lobby_screen.dart';
 import 'package:websocket_mobile/mobile/lobby/screen/scan_game_id_screen.dart';
-import 'package:websocket_mobile/mobile/user_management/model/otp_screen_arguments.dart';
 import 'package:websocket_mobile/mobile/user_management/screen/forgot_password_screen.dart';
 import 'package:websocket_mobile/mobile/user_management/screen/home_screen.dart';
 import 'package:websocket_mobile/mobile/user_management/screen/loading_screen.dart';
 import 'package:websocket_mobile/mobile/user_management/screen/login_screen.dart';
-import 'package:websocket_mobile/mobile/user_management/screen/otp_screen.dart';
 import 'package:websocket_mobile/mobile/user_management/screen/sign_up_screen.dart';
 import 'package:websocket_mobile/mobile/user_management/screen/welcome_screen.dart';
 import 'package:websocket_mobile/my_http_override.dart';
-import 'package:websocket_mobile/web/create_game/model/qr_screen_arguments.dart';
 import 'package:websocket_mobile/web/create_game/screen/create_game_screen.dart';
-import 'package:websocket_mobile/web/create_game/screen/qr_screen.dart';
 
 void main() {
   HttpOverrides.global = MyHttpOverrides();
   runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute:
-          kIsWeb ? CreateGameScreen.routeName : LoadingScreen.routeName,
-      routes: {
-        // mobile screens
-        WelcomeScreen.routeName: (context) => const WelcomeScreen(),
-        LoadingScreen.routeName: (context) => const LoadingScreen(),
-        //OtpScreen.routeName: (context) => const OtpScreen(),
-        SignUpScreen.routeName: (context) => const SignUpScreen(),
-        LoginScreen.routeName: (context) => const LoginScreen(),
-        ForgotPasswordScreen.routeName: (context) =>
-            const ForgotPasswordScreen(),
-        HomeScreen.routeName: (context) => const HomeScreen(),
-        BrowseGamesScreen.routeName: (context) => const BrowseGamesScreen(),
-        ScanGameIdScreen.routeName: (context) => const ScanGameIdScreen(),
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => ConnectedPlayerProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute:
+            kIsWeb ? CreateGameScreen.routeName : LoadingScreen.routeName,
+        routes: {
+          // mobile screens
+          WelcomeScreen.routeName: (context) => const WelcomeScreen(),
+          LoadingScreen.routeName: (context) => const LoadingScreen(),
+          //OtpScreen.routeName: (context) => const OtpScreen(),
+          SignUpScreen.routeName: (context) => const SignUpScreen(),
+          LoginScreen.routeName: (context) => const LoginScreen(),
+          ForgotPasswordScreen.routeName: (context) =>
+              const ForgotPasswordScreen(),
+          HomeScreen.routeName: (context) => const HomeScreen(),
+          BrowseGamesScreen.routeName: (context) => const BrowseGamesScreen(),
+          ScanGameIdScreen.routeName: (context) => const ScanGameIdScreen(),
 
-        // web screens
-        CreateGameScreen.routeName: (context) => const CreateGameScreen(),
-      },
-      onGenerateRoute: (settings) {
-        if (settings.name == OtpScreen.routeName) {
-          final args = settings.arguments as OtpScreenArguments?;
-
-          return MaterialPageRoute(
-            builder: (context) {
-              return OtpScreen(
-                text: args!.text,
-                username: args.username,
-                password: args.password,
-                email: args.email,
-                requestType: args.requestType,
-              );
-            },
-          );
-        } else if (settings.name == LobbyScreen.routeName) {
-          final args = settings.arguments as LobbyScreenArguments?;
-
-          return MaterialPageRoute(
-            builder: (context) {
-              return LobbyScreen(
-                gameId: args!.gameId,
-              );
-            },
-          );
-        } else if (settings.name == GameScreen.routeName) {
-          final args = settings.arguments as GameScreenArguments?;
-
-          return MaterialPageRoute(
-            builder: (context) {
-              return GameScreen(
-                webSocketService: args!.webSocketService,
-                gameId: args.gameId,
-              );
-            },
-          );
-        }
-        // web
-        else if (settings.name == QRScreen.routeName) {
-          final args = settings.arguments as QRScreenArguments?;
-
-          return MaterialPageRoute(
-            builder: (context) {
-              return QRScreen(
-                name: args!.name,
-              );
-            },
-          );
-        }
-        return null;
-      },
+          // web screens
+          CreateGameScreen.routeName: (context) => const CreateGameScreen()
+        },
+        onGenerateRoute: RouteGenerator.generateRoute,
+      ),
     ),
   );
 }
